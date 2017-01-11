462c69bbb0dbcd64ce49a1cf15f0b8d297aeddfa
brettrsa <brettrsa@gmail.com>
2017-01-11 10:44:44 +0200

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -18,8 +18,13 @@ login_payload = {'username': fortigate_user, 'secretkey': fortigate_pass}
 
 r = requests.post(login_url, data=login_payload, verify=False)
 cookiejar = r.cookies
+csrftoken = r.cookies['ccsrftoken']
 
-r = requests.get('https://%s/api/v2/monitor/system/config/backup?destination=file&scope=global' % fortigate_host, cookies=cookiejar, verify=False)
+# this was tested on a 60D running 5.4.0GA
+#r = requests.get('https://%s/api/v2/monitor/system/config/backup?destination=file&scope=global' % fortigate_host, cookies=cookiejar, verify=False)
+
+# this was tested on on multiple models running code less than 5.4.0
+r = requests.get('https://%s/downloadfile/sysconf.cfg?msg=1&CSRF_TOKEN=csrftoken&password=' % fortigate_host, cookies=cookiejar, verify=False) 
 
 output.write(r.content)
 output.close()
